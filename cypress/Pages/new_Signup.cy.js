93b735ee2d2861930e4549d58bf48c5567582bdb
PanditKusum <panditkusum2051@gmail.com>
2023-08-25 11:12:32 +0545

    invalid signup cases

--- cypress/Pages/new_Signup.cy.js
+++ cypress/Pages/new_Signup.cy.js
@@ -1,9 +1,14 @@
+import { valid_email } from "../support/commands";
+
 export class signuptest {
+  visit_signup =':nth-child(3) > .nav-link'
     new_signup_username = ':nth-child(1) > .form-control';
     new_signup_email = ':nth-child(2) > .form-control';
     new_signup_password = ':nth-child(3) > .form-control';
     new_signup_btn = '.btn';
-   // signup_assertion_error='.error-messages > li'
+    assert_btn= '.btn';
+    empty_assertion_error='.error-messages > li'
+    empty_username_error='.error-messages > li'
   
     navigate(url) {
       cy.visit(url);
@@ -14,7 +19,7 @@ export class signuptest {
     }
   
     enterEmail(email) {
-      cy.get(this.new_signup_email).type(email);
+      cy.get(this.new_signup_email).type(valid_email);
     }
   
     enterPassword(password) {
@@ -25,8 +30,21 @@ export class signuptest {
       cy.get(this.new_signup_btn).click();
     
     }
-}
-//     AssertionError (error){
-//        cy. get (signup_assertion_error)(error);
-//     }
-//   
+    
+    assertButtonIsDisabled() {
+      cy.get(this.assert_btn).should('be.disabled');
+    }
+
+      emptyassertionError(){// empty all field assertion error
+      cy.get(this.empty_assertion_error).should('contain',"please enter all required field");
+      }
+
+      emptyUsernameError(){
+        cy.get(this.empty_username_error).should('contain', "username can't be blank");
+      }
+
+      }
+    
+
+    
+
--- cypress/e2e/signupscenario.cy.js
+++ cypress/e2e/signupscenario.cy.js
@@ -1,18 +1,41 @@
 
 import { signuptest } from "../Pages/new_Signup.cy"; // Adjust the import path and name
 
-describe('Sign Up Tests', function () {
-  const signupInstance = new signuptest();
+// for empty field scenarios
 
-  it('should signup successfully', function () {
+const signupInstance = new signuptest();
+
+describe('empty_case', function () {
+it('should disabeled button', function () {
     signupInstance.navigate('https://next-realworld.vercel.app/');
     cy.get(':nth-child(3) > .nav-link').click()
-    signupInstance.enterUsername('krizma');
+    cy .wait (100);
+    signupInstance.enterUsername.clear;
     signupInstance.enterEmail.clear;
-    signupInstance.enterPassword('pandit@123')
+    signupInstance.enterPassword.clear;
     signupInstance.clickButton();
-    cy.wait (1000);
-    //signupInstance.AssertionError("email can't be blank");
+    signupInstance.assertButtonIsDisabled();
+    // user must see error message 
+    signupInstance.emptyassertionError();
+
 
   })
 });
+// empty email and password with username input field
+describe('emptyusername', function () {
+  it('should display error message ', function () {
+      signupInstance.navigate('https://next-realworld.vercel.app/');
+      cy.get(':nth-child(3) > .nav-link').click()
+      cy .wait (100);
+      signupInstance.enterUsername.clear;
+      signupInstance.enterEmail('valid_email');
+      signupInstance.enterPassword('Testee@123');
+      signupInstance.clickButton();
+      
+      // user must see error message 
+      signupInstance.emptyUsernameError();
+  
+  
+    })
+  })
+
